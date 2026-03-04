d8dd97fd21b8fe78a6b7af99253a29740179e023
Sophia Roque Martins <martinssophia983@gmail.com>
2026-03-04 17:49:59 -0300

    upadate usuario

--- src/usuario_crud.php
+++ src/usuario_crud.php
@@ -3,10 +3,12 @@
 
 // Função para buscar todos os usuários do banco de dados
 //PDO $conexao - Objeto de conexão com o banco de dados
+//processo de dados 
 function buscarUsuario(PDO $conexão)
 {
 
-// Define uma string contendo o comando SQL que será enviado ao banco de dados.
+// Define uma string contendo o comando SQL que será enviado ao banco de dados. 
+//1 coisa a fazer para ter uma tabela de banco de dados aparecendo no navegador.//intup
 // Seleciona as colunas id, nome e email da tabela 'usuarios' ordenando pela coluna 'nomes' 
 $sql = "SELECT id,nome,email FROM usuarios ORDER BY nome";
 //$stmt
@@ -22,9 +24,10 @@ $consulta->execute();
 
 
 // Retorna o resultado da consulta para quem chamou a função.
-// fetch() captura apenas UMA linha (o primeiro registro encontrado).
-// PDO::FETCH_ASSOC organiza os dados em um array associativo, 
-// onde os índices são os nomes das colunas (ex: $usuario['nome']).
+// retorna um array contendo todas os registros remanescentes no conjunto de resultados
+//O array representa cada registro como um array de valores de coluna ou um objeto com propriedades correspondentes a cada nome de coluna.
+// Um array vazio é retornado se não houver resultados na busca.
+// input -> processamento -> output
 return $consulta->fetchAll(PDO:: FETCH_ASSOC);
 }
 
